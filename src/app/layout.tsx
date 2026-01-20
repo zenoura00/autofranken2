3b7197b037fd73f8a33826258c168c9175a807e6
Abdulla <abdulla@MacBook-Pro-von-Abdulla.local>
2026-01-20 22:23:02 +0100

    Update: description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import ClientBody from "./ClientBody";
+import { ClientBody } from "./ClientBody";
 import Script from "next/script";
 
 const geistSans = Geist({
